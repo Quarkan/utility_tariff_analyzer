0011210a6514c2a49f9b579814ec5853a3de5abf
sergey <a89397074218@gmail.com>
2024-10-12 21:20:43 +0300

    it is my new program utility analyzer tariff

---
+++ SQL_tariff.py
@@ -0,0 +1,77 @@
+import sqlite3 as sq
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedFormatter,MaxNLocator,LinearLocator
+
+
+with sq.connect('indications_tariff.db') as sun:
+    cur = sun.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS data')
+    cur.execute("""CREATE TABLE IF NOT EXISTS data
+    (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    UNIT_OF_MEASUREMENT TEXT,
+    age INTEGER,
+    name TEXT,
+    cost INTEGER
+
+    )
+    """)
+    #формируем базу данных
+
+    #2014
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2014,'отопление',3393.68)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2014,'вода',29.56)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2014,'Электроэнергия',4.57)")
+
+    #2015
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2015,'отопление',3555.34)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2015,'вода',30.79)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2015,'Электроэнергия',4.76)")
+
+    #2016
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2016,'отопление',3836.18)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2016,'вода',34.19)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2016,'Электроэнергия',5.24)")
+
+    #2017
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2017,'отопление',3967.16)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2017,'вода',36.02)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2017,'Электроэнергия',5.75)")
+
+    #2018
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2018,'отопление',4117.02)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2018,'вода',39.92)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2018,'Электроэнергия',6.09)")
+
+    #2019
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2019,'отопление',4322.4)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2019,'вода',43.38)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2019,'Электроэнергия',4.57)")
+
+    #2020
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2020,'отопление',4450.8)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2020,'вода',44.13)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2020,'Электроэнергия',4.81)")
+
+    #2021
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2021,'отопление',4605.6)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2021,'вода',45.81)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2021,'Электроэнергия',5.02)")
+
+    #2022
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2022,'отопление',4742.4)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2022,'вода',71.54)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2022,'Электроэнергия',7.58)")
+
+    #2023
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2023,'отопление',5924.04)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2023,'вода',106.03)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2023,'Электроэнергия',8.41)")
+
+    #2024
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 Гкал',2024,'отопление',6587.44)")
+    cur.execute("INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за 1 м3',2024,'вода',116.56)")
+    cur.execute( "INSERT INTO data(UNIT_OF_MEASUREMENT,age,name,cost) VALUES('Руб. за кВт.ч',2024,'Электроэнергия',9.68)")
+
---
+++ analyzer_tariff.py
@@ -0,0 +1,64 @@
+import sqlite3 as sq
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import matplotlib.gridspec as gridspec
+
+with sq.connect('indications_tariff.db') as sun:
+    cur = sun.cursor()
+
+    # вывод данных в analyzer_tariff
+    cur.execute("SELECT age FROM data WHERE name LIKE 'отопление'")
+    result_const = cur.fetchall()
+    cur.execute("SELECT cost FROM data WHERE name LIKE 'отопление'")
+    result_1 = cur.fetchall()
+    cur.execute("SELECT cost FROM data WHERE name LIKE 'вода'")
+    result_2 = cur.fetchall()
+    cur.execute("SELECT cost FROM data WHERE name LIKE 'Электроэнергия'")
+    result_3 = cur.fetchall()
+
+    result_list_const = []
+    result_list_1 = []
+    result_list_2 = []
+    result_list_3 = []
+    result = []
+
+    #сумма всех комунальных услуг
+    for cost in result_1:
+        result_list_1.append(cost[0])
+    for age in result_const:
+        result_list_const.append(age[0])
+    for cost in result_2:
+        result_list_2.append(cost[0])
+    for cost in result_3:
+        result_list_3.append(cost[0])
+    for i in range(11):
+        ress = (result_list_1[i] + result_list_2[i] + result_list_3[i])
+        oct_ress = round(ress,2)
+        result.append(oct_ress)
+#отправляем последние изменения
+sun.commit()
+
+f1 = plt.figure(figsize=(7,4))
+f1.set_facecolor('#eee')
+gs = gridspec.GridSpec(2,3,figure=f1)
+
+ax = f1.add_subplot(gs[1,:])
+
+y = np.array([result[i] for i in range(11)])
+x = np.array([result_list_const[i] for i in range(11)])
+
+ax.xaxis.set_major_locator(MaxNLocator(13))
+
+ax.plot(x, y, 'r-o')
+
+for i,v in enumerate(result):
+    plt.text(x=x[i],y=v,s=str(v)+'₽',ha='center')
+
+ax.set_title('COST TARIFF IN RUB')
+ax.set_ylabel('cost')
+ax.set_xlabel('age')
+plt.grid()
+plt.show()
+
+
